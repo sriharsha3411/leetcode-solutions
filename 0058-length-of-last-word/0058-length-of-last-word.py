71cf163274a4a1478c3d95a4d6a18b7963939976
sriharsha3411 <155721612+sriharsha3411@users.noreply.github.com>
2025-07-08 17:34:03 +0530

    Time: 0 ms (100%), Space: 17.9 MB (37.73%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -4,18 +4,17 @@ class Solution:
         count=1
         char_count=0
         for i in range(len(s)-1,-1,-1):
-
-            if char_count==1 and s[i]==" ":
-                break
-                
+            
             if s[i].isalpha() and char_count==0:
                 char_count+=1
                 continue
 
+            if char_count==1 and s[i]==" ":
+                break
+
             if s[i].isalpha() and char_count==1:
                 count+=1
 
-            
-
+        
         return count
 

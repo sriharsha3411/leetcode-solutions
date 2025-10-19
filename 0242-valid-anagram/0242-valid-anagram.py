3f50ef4f356f75d5d80923fcee1d85bba8e0b3e9
sriharsha3411 <155721612+sriharsha3411@users.noreply.github.com>
2025-10-20 01:07:39 +0530

    Time: 10 ms (74.5%), Space: 17.8 MB (86.23%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,10 +1,23 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         
-        if len(s)==len(t):
-            if sorted(s)==sorted(t):
-                return True
-            else:
-                return False
-                
-        return False
+        # if len(s)==len(t):
+        #     if sorted(s)==sorted(t):
+        #         return True
+        #     else:
+        #         return False
+
+        # return False
+
+        s_hashmap={}
+        for char in s:
+            s_hashmap[char]=s_hashmap.get(char,0)+1
+        t_hashmap={}
+        for char in t:
+            t_hashmap[char]=t_hashmap.get(char,0)+1
+        
+        if s_hashmap==t_hashmap:
+            return True
+        else:
+            return False
+        
